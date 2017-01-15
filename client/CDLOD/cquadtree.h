ea2296b3bb430fd228d9930ebc39717df5cb0dfd
maked0n <alex.mikhalevich@gmail.com>
2017-01-15 19:40:48 +0000

    Documented

--- client/CDLOD/cquadtree.h
+++ client/CDLOD/cquadtree.h
@@ -8,12 +8,25 @@
 #include "utility.h"
 
 namespace CDLOD {
+    /*!
+     * \brief The CQuadtree class
+     * Quadtree implementation.
+     */
     class CQuadtree {
     private:
-        QScopedPointer<CQuadtreeNode> m_root;
+        QScopedPointer<CQuadtreeNode> m_root;       ///< Pointer to the root node in the tree
+        QVector<qreal> m_lod_vis_distance_ratios;   ///< LOD visibility range distribution
     public:
-        CQuadtree();
+        CQuadtree() {}
+        /*!
+         * \brief           Initialises the tree
+         * \param[in]       settings_ptr        different system settings
+         */
         void init(QScopedPointer<CSettings>& settings_ptr);
+        /*!
+         * \brief           Launches a recursive tree traversal
+         * \param[out]      selection           CSelection instance which contains all information needed to render the terrain
+         */
         void select(CSelection& selection);
     };
 }
