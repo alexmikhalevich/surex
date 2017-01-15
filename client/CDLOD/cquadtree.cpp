882647860f2ae8525c3ab930815cb5ecaceacde1
maked0n <alex.mikhalevich@gmail.com>
2017-01-15 19:40:12 +0000

    LOD visibility range distribution calculation implemented

--- client/CDLOD/cquadtree.cpp
+++ client/CDLOD/cquadtree.cpp
@@ -2,11 +2,13 @@
 
 using namespace CDLOD;
 
-CQuadtree::CQuadtree() {
-}
-
 void CQuadtree::init(QScopedPointer<CSettings>& settings_ptr) {
-
+    qreal detail_balance = 1.0;
+    qreal coeff = settings_ptr->lod_level_distance_ratio();
+    for(int i = 0; i < settings_ptr->max_lod_level(); ++i) {
+        m_lod_vis_distance_ratios[i] = detail_balance;
+        detail_balance *= coeff;
+    }
 }
 
 void CQuadtree::select(CSelection& selection) {
