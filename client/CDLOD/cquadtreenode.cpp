d9e4949caea5827ac380657ca67e2f0dbfa2e76d
maked0n <alex.mikhalevich@gmail.com>
2017-01-15 18:15:38 +0000

    Commented temporary unnecessary code

--- client/CDLOD/cquadtreenode.cpp
+++ client/CDLOD/cquadtreenode.cpp
@@ -55,13 +55,12 @@ ESelectionResult CQuadtreeNode::select(CSelection& selection, bool parent_in_fru
         short lod_level = selection.max_lod_level() - m_level;
         //FIXME: do we really need to create new CQuadtreeNode? Maybe it's possible to push _this_ to the selection
         selection.push_node(QSharedPointer<CQuadtreeNode>(new CQuadtreeNode(lod_level, selection.settings())));
-
-        if(m_level != 0) {
-            qreal camera_distance = qSqrt(bounding_box.max_distance_squared(selection.observer_position()));
-            qreal morph_start = selection.morph_start(selection.max_lod_level() - m_level + 1);
-            if(camera_distance > morph_start)
-                selection.set_small_visible_distance(true);
-        }
+//        if(m_level != 0) {
+//            qreal camera_distance = qSqrt(bounding_box.max_distance_squared(selection.observer_position()));
+//            qreal morph_start = selection.morph_start(selection.max_lod_level() - m_level + 1);
+//            if(camera_distance > morph_start)
+//                selection.set_small_visible_distance(true);
+//        }
         return ESelectionResult::SELECTED;
     }
     if(top_left_result == ESelectionResult::SELECTED || top_right_result == ESelectionResult::SELECTED
