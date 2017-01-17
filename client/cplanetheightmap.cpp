84aa170be48bf073f4843cd26dd745b7e4b72b45
maked0n <alex.mikhalevich@gmail.com>
2017-01-17 21:48:07 +0000

    Heightmap representation

---
+++ client/cplanetheightmap.cpp
@@ -0,0 +1,58 @@
+#include "cplanetheightmap.h"
+
+CPlanetHeightmap::CPlanetHeightmap(int seed, const QSharedPointer<CSettings>& settings,
+                                   qreal height_min, qreal height_max) {
+    m_seed = seed;
+    m_noise_octaves = settings->noise_octaves();
+    m_noise_persistence = settings->noise_persistence();
+    m_noise_scale = m_settings->noise_scale();
+    m_height_min = height_min;
+    m_height_max = height_max;
+}
+
+CPlanetHeightmap::CPlanetHeightmap(const QString& path, const QSharedPointer<CSettings>& settings,
+                                   qreal height_min, qreal height_max) {
+    m_heightmap = QImage(path);
+    m_noise_octaves = settings->noise_octaves();
+    m_noise_persistence = settings->noise_persistence();
+    m_noise_scale = m_settings->noise_scale();
+    m_height_min = height_min;
+    m_height_max = height_max;
+}
+
+void CPlanetHeightmap::generate(qreal planet_radius) {
+    //TODO
+}
+
+QSize CPlanetHeightmap::size() const {
+    return m_heightmap.size();
+}
+
+qreal CPlanetHeightmap::height(qreal polar_angle, qreal azimut_angle) const {
+    if(m_heightmap.isNull()) {
+        //TODO
+    }
+    else {
+        qreal x = (azimut_angle / MAX_AZIMUT_ANGLE) * m_heightmap.width();
+        qreal y = (polar_angle / MAX_POLAR_ANGLE) * m_heightmap.height();
+        return _color_to_height(m_heightmap.pixelColor((int)x, (int)y));
+    }
+}
+
+qreal CPlanetHeightmap::height(const QVector3D& position) const {
+    if(m_heightmap.isNull()) {
+        return m_noise.scaled_octave_noise_3d(m_noise_octaves, m_noise_persistence, m_noise_scale,
+                                       m_height_min, m_height_max, position.x(), position.y(), position.z());
+    }
+    else {
+        //TODO
+    }
+}
+
+int CPlanetHeightmap::seed() const {
+    return m_seed;
+}
+
+void CPlanetHeightmap::serialize(const QString& filename) {
+    //TODO
+}
---
+++ client/cplanetheightmap.h
@@ -0,0 +1,73 @@
+#ifndef CPLANETHEIGHTMAP_H
+#define CPLANETHEIGHTMAP_H
+
+#include <qt5/QtGui/QImage>
+#include <qt5/QtCore/QVector>
+#include <qt5/QtGui/QColor>
+#include "iserializable.h"
+#include "csimplexnoise.h"
+#include "csettings.h"
+
+#define MAX_POLAR_ANGLE 180.0
+#define MAX_AZIMUT_ANGLE 360.0
+
+/*!
+ * \brief The CPlanetHeightmap class
+ * Heightmap representation
+ */
+class CPlanetHeightmap : public ISerializable {
+private:
+    qreal m_noise_persistence;              ///<
+    qreal m_noise_scale;                    ///<
+    qreal m_noise_octaves;                  ///<
+    qreal m_height_min;                     ///<
+    qreal m_height_max;                     ///<
+    QImage m_heightmap;                     ///< Holds heightmap image
+    int m_seed;                             ///< Seed, necessary for simplex noise generation algorithm
+    CSimplexNoise m_noise;                  ///< Simplex noise object for generating random landscape
+
+    /*!
+     * \brief Maps the heightmap color to the height value
+     * \param[in]       color
+     * \return          height
+     */
+    qreal _color_to_height(const QColor& color) const;
+public:
+    CPlanetHeightmap(int seed, const QSharedPointer<CSettings>& settings, qreal height_min, qreal height_max);
+    CPlanetHeightmap(const QString& path, const QSharedPointer<CSettings>& settings, qreal height_min, qreal height_max);
+    /*!
+     * \brief Saves all current heightmap data
+     * \param           filename            file which will contain the heilghtmap
+     */
+    virtual void serialize(const QString& filename);
+    /*!
+     * \brief Generate heightmap for the particular planet radius
+     * \param[in]       planet_radius
+     */
+    void generate(qreal planet_radius);
+    /*!
+     * \brief Returns heightmap size
+     * \return          heightmap size
+     */
+    QSize size() const;
+    /*!
+     * \brief Calculates height for the particular point on the sphere
+     * \param[in]       polar_angle
+     * \param[in]       azimut_angle
+     * \return          height
+     */
+    qreal height(qreal polar_angle, qreal azimut_angle) const;
+    /*!
+     * \brief Calculates height for the particular point on the sphere
+     * \param[in]       position        cartesian coordinates of the processed point
+     * \return
+     */
+    qreal height(const QVector3D& position) const;
+    /*!
+     * \brief Returns seed which was used for simplex noise generation
+     * \return          seed
+     */
+    int seed() const;
+};
+
+#endif // CPLANETHEIGHTMAP_H
